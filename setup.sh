83fbe379614a598e7f622e27e466aa505bee9fb2
kg <kylgrvs@gmail.com>
2015-05-26 17:04:41 -0700

    adds .alsa.state file, bashrc update for it
    also updates setup.sh and update.sh scripts

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 # copy all dot files into home directory
 # TODO look into using symbolic links
+cp .alsa.state ~
 cp .bash_profile ~
 cp .bashrc ~
 cp .gemrc ~
 cp .gitconfig ~
+cp .npmrc ~
 cp .profile ~
 cp .tundra ~
 cp .vimrc ~
-cp .npmrc ~
 
 # since I'm running linux, I don't need mjolnir
 # cp -r .mjolnir ~
--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 # copy all dotfiles back to repo directory
+cp ~/.alsa.state .
 cp ~/.bash_profile .
 cp ~/.bashrc .
 cp ~/.gemrc .
 cp ~/.gitconfig .
+cp ~/.npmrc .
 cp ~/.profile .
 cp ~/.tundra .
 cp ~/.vimrc .
-cp ~/.npmrc .
